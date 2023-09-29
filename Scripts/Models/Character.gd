4eb858e6bf608ae19272bc6131ff2473eab970dc
ford-narongrit <narongrit.th@ku.th>
2023-09-30 00:23:45 +0700

    add basic movemoent

---
+++ Scripts/Models/Character.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Character
+
+@export var max_health: float
+@export var current_health: float
+@export var max_speed: float
+@export var current_speed: float
+@export var attack_point: float
+@export var jump_force: float
